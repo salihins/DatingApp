1e767d088e72aad74c1c0d57966ac3de8645dd24
salihins <96544453+salihins@users.noreply.github.com>
2024-06-10 23:41:35 +0300

    Commit end of sec3 angular commit

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit {
     this.http.get('https://localhost:5001/api/users').subscribe({
       next: response => this.users = response,
       error: error => console.log(error),
-      complete: () => console.log('completed') 
+      complete: () => console.log('completed successfully') 
     });
   }
   
