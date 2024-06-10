4fdcc6d875411749ec94b28f0ab8bf88ff8668e4
salihins <96544453+salihins@users.noreply.github.com>
2024-06-10 23:37:54 +0300

    End of section 3. Angular project created.

--- API/Program.cs
+++ API/Program.cs
@@ -9,12 +9,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddControllers();
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
